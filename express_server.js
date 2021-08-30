6781896027e58982178dda07ed7bb197bf7bfd48
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-30 21:15:48 +0000

    added a get request for /urls.json that prints out our urlDatabase object

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (reg, res) => {
   res.send("Hello");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDataBase);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
