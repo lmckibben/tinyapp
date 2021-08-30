a6bff78584a6d82ab64a5059c9a07d6048496c31
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-30 21:11:11 +0000

    Created express_server.js and set http server using express that says hello when client sends request

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const urlDataBase = {
+  "b2xVn2": "http://www.lighthouse.ca",
+  "9sm5xk": "http//www.google.ca"
+};
+
+app.get("/", (reg, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
