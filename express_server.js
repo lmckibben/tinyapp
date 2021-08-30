c8066d3bd93b6cd4ed73ccaea81416861dfec4b0
Leland Mckibben <lmckibben28@hotmail.com>
2021-08-30 21:20:03 +0000

    added get request for /hello that sends html Hello World

--- express_server.js
+++ express_server.js
@@ -15,6 +15,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDataBase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
