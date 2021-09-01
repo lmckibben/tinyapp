5097efbfeaf9ff52e0957f403e2a96094d181054
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-01 20:43:51 +0000

    update code for no pass or email to 401,added logic to see if email is already in user database and if it is to send 401 error

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const e = require('express');
 
 const app = express();
 const port = 8080;
@@ -54,6 +55,16 @@ const users = {
   }
 }
 
+const getUserEmail = (email) => {
+  for (const key in users) {
+    const user = users[key];
+    if (user.email === email) {
+      return user
+    }
+  }
+  return null
+};
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -75,17 +86,22 @@ app.get("/register", (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  res.cookie('user_id', req.body.email);
+  const foundUserEmail = getUserEmail(req.body.email);
   if (req.body.email.length === 0 || req.body.password.length === 0) {
-    res.send(res.statusCode = 400);
+    res.send(res.statusCode = 401);
+  } else if (foundUserEmail) {
+    res.send(res.statusCode = 401); 
+  } else {
+    res.cookie('user_id', req.body.email);
+    const user_id = generateRandomString();
+    users[user_id] = {
+      id: user_id,
+      email: req.body.email,
+      password: req.body.password
+    }
+    res.redirect('/urls');
   }
-  const user_id = generateRandomString();
-  users[user_id] = {
-    id: user_id,
-    email: req.body.email,
-    password: req.body.password
-  }
-  res.redirect('/urls');
+  console.log('users', users);
 });
 
 app.post("/login", (req, res)=> {
