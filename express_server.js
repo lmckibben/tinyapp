bc8cce83110c1694b47d49f7c08cf78cb8595772
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-01 01:19:32 +0000

    added POST request to logout

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.post("/login", (req, res)=> {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("/urls");
+});
+
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
