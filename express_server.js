c3dc0ca3d99fe7d60fa4a39e6557c38e6dc4978b
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-01 21:29:45 +0000

    updated /login POST request to use userdatabase and create new user_id cookie. updated logout request to use new user_id cookie

--- express_server.js
+++ express_server.js
@@ -112,13 +112,17 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res)=> {
-  const username = req.body.username;
-  res.cookie('username', username);
-  res.redirect("/urls");
+  const foundUserEmail = getUserEmail(req.body.email);
+  if (foundUserEmail) {
+    res.cookie('user_id', req.body.email);
+    res.redirect("/urls");
+  } else {
+    res.send(res.statusCode = 401);
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls");
 });
 
