594152a1f4652a0c740270878649e0d6a7421a3d
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-02 16:20:09 +0000

    required in package bcryptjs

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const e = require('express');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 8080;
