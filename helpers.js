9ac6a626230d8108b113af58123c21ecf1fe1481
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-04 23:14:07 +0000

    added in comments to explain the logic of generateRandomString

--- helpers.js
+++ helpers.js
@@ -22,19 +22,26 @@ const generateRandomString = function() {
   let output = "";
   let i = 0;
   while (i < 6) {
+    //gives random number from 1-3
     let ranNum = Math.floor(Math.random() * 3) + 1;
     if (ranNum === 1) {
+      //give random number between 48-57
       let ranCharCode = Math.floor(Math.random() * (57 - 48) + 48);
+      //turns the number number into character code equivalent 0-9
       let ranChar = String.fromCharCode(ranCharCode);
       output += ranChar;
     }
     if (ranNum === 2) {
+      //give random number between 65-90
       let ranCharCode = Math.floor(Math.random() * (90 - 65) + 65);
+      //turns the number number into character code equivalent a-z
       let ranChar = String.fromCharCode(ranCharCode);
       output += ranChar;
     }
     if (ranNum === 3) {
+      //give random number between 97-122
       let ranCharCode = Math.floor(Math.random() * (122 - 97) + 97);
+      //turns the number number into character code equivalent A-Z
       let ranChar = String.fromCharCode(ranCharCode);
       output += ranChar;
     }
