d074367d30499b57280ebbeee99b3abe94f01e9f
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-04 23:12:09 +0000

    created new file to store the urldatabase.js

---
+++ urlDatabase.js
@@ -0,0 +1,12 @@
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouse.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xk": {
+    longURL: "http//www.google.ca",
+    userID: "userRandomID"
+  }
+};
+
+module.exports = urlDatabase;
